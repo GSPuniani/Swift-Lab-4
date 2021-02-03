63588b6e9b740b8c4e8fa5b8c5b1ceac745241e5
Gobind Puniani <gobindpuniani@gmail.com>
2021-02-03 01:49:34 -0800

    Finished first page on Structs

--- Lab4.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ Lab4.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -4,26 +4,45 @@ import Foundation
  
  Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
  */
-
+struct GPS {
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+}
 
 /*:
  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
  */
-
-
+var somePlace = GPS()
+print(somePlace.latitude) //0.0
+print(somePlace.longitude) //0.0
 /*:
  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
  */
 
-
+somePlace.latitude = 51.514004
+somePlace.longitude = 0.125226
+print(somePlace.latitude) //51.514004
+print(somePlace.longitude) //0.125226
 /*:
  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
  */
-
+struct Book {
+    var title: String = ""
+    var author: String = ""
+    var pages: Int = 0
+    var price: Double = 0.0
+}
 
 /*:
  Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then, using the properties of `favoriteBook`, print out facts about the book.
  */
+var favoriteBook = Book()
+print(favoriteBook.title) //""
 
+favoriteBook.title = "Sapiens: A Brief History of Humankind"
+favoriteBook.author = "Yuval Noah Harari"
+favoriteBook.pages = 443
+favoriteBook.price = 14.99
 
+print("My favorite book is '\(favoriteBook.title)' by \(favoriteBook.author). It is \(favoriteBook.pages) pages long and costs $\(favoriteBook.price).")
 //: [Next](@next)
